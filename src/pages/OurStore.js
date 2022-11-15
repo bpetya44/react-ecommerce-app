264a8a6ec5eeafa16552c9f3e75b1565302b2060
Petya Angelova <bedfordpetya@gmail.com>
2022-11-15 17:29:13 +0200

    Our store page right side

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,8 +1,15 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
+import ReactStars from "react-rating-stars-component";
+import { useState } from "react";
+import ProductCard from "../components/ProductCard"
+
 
 const OurStore = () => {
+  const [grid, setGrid] = useState(4);
+// alert(grid);
+
   return (
     <>
       <Meta title={"Our Store"} />
@@ -131,42 +138,129 @@ const OurStore = () => {
                 <h3 className="filter-title">Product Tags</h3>
                 <div>
                   <div className="product-tags d-flex flex-wrap align-items-center gap-10">
-                <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
-                  Headphones
-                </span>
-                <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
-                  Laptops
-                </span>
-                <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
-                  Phones
-                </span>
-                <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
-                  Tablets
-                </span>
-                <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
-                  Speakers
-                </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Headphones
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Laptops
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Phones
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Tablets
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Speakers
+                    </span>
                   </div>
                 </div>
               </div>
-
 
               <div className="filter-card mb-3">
                 <h3 className="filter-title">Random Products</h3>
                 <div>
                   <div className="random-products d-flex">
                     <div className="w-50">
-                      <img className="img-fluid" src="images/smart.png" alt="smart watch" />
+                      <img
+                        className="img-fluid"
+                        src="images/smart.png"
+                        alt="smart watch"
+                      />
                     </div>
                     <div className="w-50">
                       <h5>Best Watch for sport</h5>
+
+                      <ReactStars
+                        count={5}
+                        size={28}
+                        value={3}
+                        edit={false}
+                        emptyIcon={<i className="far fa-star"></i>}
+                        halfIcon={<i className="fa fa-star-half-alt"></i>}
+                        fullIcon={<i className="fa fa-star"></i>}
+                      />
+                      <p>$299</p>
                     </div>
-                    <p>$299</p>
+                  </div>
+
+                  <div className="random-products d-flex mt-3">
+                    <div className="w-50">
+                      <img
+                        className="img-fluid"
+                        src="images/smart.png"
+                        alt="smart watch"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>Best Watch for sport</h5>
+
+                      <ReactStars
+                        count={5}
+                        size={28}
+                        value={3}
+                        edit={false}
+                        emptyIcon={<i className="far fa-star"></i>}
+                        halfIcon={<i className="fa fa-star-half-alt"></i>}
+                        fullIcon={<i className="fa fa-star"></i>}
+                      />
+                      <p>$299</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="col-9"></div>
+
+            <div className="col-9">
+              <div className="filter-sort-grid mb-3">
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="mb-0" style={{"width":"100px"}}>Sort By:</p>
+                    <select className="form-control form-select" name="" id="">
+                      <option value="manual">Featured</option>
+                      <option value="best-selling" selected="selected">
+                        Best Selling
+                      </option>
+                      <option value="title-ascending">
+                        Alphabetically, A-Z
+                      </option>
+                      <option value="title-descending">
+                        Alphabetically, Z-A
+                      </option>
+                      <option value="price-ascending">
+                        Price, low to high
+                      </option>
+                      <option value="price-descending">
+                        Price, high to low
+                      </option>
+                      <option value="created-ascending">
+                        Date, old to new
+                      </option>
+                      <option value="created-descending">
+                        Date, new to old
+                      </option>
+                    </select>
+                  </div>
+
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="total-products mb-0">20 Products</p>
+                    <div className="d-flex align-items-center gap-10 grid">
+                      <img onClick={()=>setGrid(4)} src="images/gr4.svg" 
+                      className="d-block img-fluid" alt="grid" />
+                      <img onClick={()=>setGrid(3)} src="images/gr3.svg" alt="grid" />
+                      <img onClick={()=>setGrid(2)} src="images/gr2.svg" alt="grid" />
+                      <img onClick={()=>setGrid(1)} src="images/gr.svg" alt="grid" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="product-list py-3">
+                <ProductCard grid={grid}/>
+              </div>
+
+
+            </div>
           </div>
         </div>
       </div>
