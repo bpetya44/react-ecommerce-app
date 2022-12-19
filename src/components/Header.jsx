57d80fbadc87f106585f5139a2cb0913baff5757
Petya Angelova <bedfordpetya@gmail.com>
2022-12-20 01:23:54 +0200

    header part 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import { NavLink, Link } from 'react-router-dom'
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <header className="header-top-strip py-3">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-6">
+            <p className='text-white mb-0'>Free Shipping Over $100 & Free Returns</p>
+          </div>
+          <div className="col-6">
+           <p className='text-end text-white mb-0'> Hotline: <a className='text-white' href="tel:+359876711314">+359 876711314</a></p>
+          </div>
+        </div>
+      </div>
+    </header>
+    
+    </>
   )
 }
 
